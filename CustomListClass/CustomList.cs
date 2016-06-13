d8b91a538384eb21c76ea82b31cb4dfbd11cc6ec
kuzmicheff <kuzmicheff@gmail.com>
2016-06-13 18:41:30 -0500

    Custom list class using System.Collections.Generic in C#
    This version allows adding new items to the CustomListClass lists and printing list items to console.

---
+++ CustomListClass/CustomList.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomListClass
+{
+    class CustomList<T> : IEnumerable
+    {
+        T[] customList;
+
+        public CustomList()
+        {
+            customList = new T[0];
+        }
+
+        public CustomList(T[] argumentValue)
+        {
+            this.customList = argumentValue;
+        }
+
+        public void AddToList(T argumentValue)
+        {
+            T[] tempList;
+            tempList = new T[customList.Length + 1];
+            for (int i = 0; i < customList.Length; i++)
+            {
+                tempList[i] = customList[i];
+            }
+            tempList[tempList.Length - 1] = argumentValue;
+            customList = tempList;
+        }
+
+        public void PrintList()
+        {
+            foreach (T item in customList)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            for (int i = 0; i < customList.Length; i ++)
+            {
+                yield return customList[i];
+            }
+            
+        }
+    }
+}
---
+++ CustomListClass/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomListClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomList<int> intSampleList = new CustomList<int>();
+            intSampleList.AddToList(5);
+            intSampleList.AddToList(15);
+            intSampleList.AddToList(25);
+            CustomList<string> stringSampleList = new CustomList<string>();
+            stringSampleList.AddToList("Adam");
+            stringSampleList.AddToList("Andrew");
+            stringSampleList.AddToList("Mike");
+            intSampleList.PrintList();
+            stringSampleList.PrintList();
+
+            Console.WriteLine("Press any key to quit:");
+            Console.ReadKey();
+        }
+    }
+}
